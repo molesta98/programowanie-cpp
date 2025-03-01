6b40369a4b7b2a8a43b11909cb47212a2ec65b4c
Daniel Molenda <daniel.molenda@shoper.pl>
2025-03-01 10:27:52 +0100

    🔧 Add comments

--- lab1/task6.cpp
+++ lab1/task6.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+// Napisz program, który pobierze dwie liczby od użytkownika,
+// wyświetli ich sumę, a następnie poinformuje użytkownika,
+// czy wynik jest liczbą parzystą czy nieparzystą.
+
 int main() {
   short first_number, second_number;
   short sum = 0;
