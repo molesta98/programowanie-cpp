73de8e38af5aac0576ac7ea21c0cd612b49c961e
Daniel Molenda <daniel.molenda@shoper.pl>
2025-03-01 09:26:18 +0100

    🔧 Task1, Task2, Task3 - done

---
+++ lab1/task2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  int a = 5;
+  int b = 3;
+
+  std::cout << "Liczba a: " << a << std::endl;
+  std::cout << "Liczba b: " << b << std::endl;
+  std::cout << "Suma: " << a + b << std::endl;
+  std::cout << "Różnica: " << a - b << std::endl;
+  std::cout << "Iloczyn: " << a * b << std::endl;
+
+  return 0;
+}
