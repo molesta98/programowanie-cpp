88f48e1ca6c1865708eba2d5b38de3d634358ba7
Daniel Molenda <daniel.molenda@shoper.pl>
2025-03-01 10:46:31 +0100

    🔧 Add error handling

--- lab2/task1.cpp
+++ lab2/task1.cpp
@@ -7,16 +7,21 @@ int main() {
   std::cout << "Podaj wynik egzaminu (liczba punktow): ";
   std::cin >> exam_result;
 
-  if (exam_result >= 50 && exam_result <= 69)
-    grade = "Dostateczna";
-  else if (exam_result >= 70 && exam_result <= 84)
-    grade = "Dobra";
-  else if (exam_result >= 85 && exam_result <= 99)
-    grade = "Bardzo dobra";
-  else if (exam_result == 100)
-    grade = "Celujaca";
-  else
+  if (exam_result >= 0 && exam_result <= 49) {
     grade = "Niedostateczna";
+  } else if (exam_result >= 50 && exam_result <= 69) {
+    grade = "Dostateczna";
+  } else if (exam_result >= 70 && exam_result <= 84) {
+    grade = "Dobra";
+  } else if (exam_result >= 85 && exam_result <= 99) {
+    grade = "Bardzo dobra";
+  } else if (exam_result == 100) {
+    grade = "Celujaca";
+  } else {
+    std::cout << "Podano niepoprawny wynik egzaminu." << std::endl;
+    return 1;
+  }
 
   std::cout << "Ocena: " << grade << std::endl;
+  return 0;
 }
