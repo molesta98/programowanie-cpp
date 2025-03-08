708429bcfcaa75dc1952104ab4e03e032763a48d
Daniel Molenda <daniel.molenda@shoper.pl>
2025-03-08 17:40:22 +0100

    📝 Update comments

--- lab2/petla_for/task1.cpp
+++ lab2/petla_for/task1.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+/*
+ * Napisz program, który wyświetli wszystkie liczby parzyste od 2 do N,
+ * gdzie N będzie podane przez użytkownika. Użyj pętli while, aby wykonać to
+ * zadanie.
+ * - Poproś użytkownika o podanie liczby całkowitej dodatniej N.
+ * - Użyj pętli while, aby sprawdzić i wyświetlić wszystkie liczby parzyste od 2
+ * do N na ekranie.
+ * - Po wyświetleniu wszystkich liczb parzystych zakończ działanie programu.
+ */
 int main() {
   short userValueN, sum = 0;
   const short BEGIN_VALUE = 1;
