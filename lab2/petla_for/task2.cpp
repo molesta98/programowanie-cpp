583020e581fef6912a0e780fae3b70649edd7110
Daniel Molenda <daniel.molenda@shoper.pl>
2025-03-08 17:44:30 +0100

    🐛 Fix dir

--- lab2/petla_for/task2.cpp
+++ lab2/petla_for/task2.cpp
@@ -1,14 +1,5 @@
 #include <iostream>
 
-/*
- * Napisz program, który obliczy sumę wszystkich liczb naturalnych od 1 do N,
- * gdzie N będzie liczbą podaną przez użytkownika. Użyj pętli while, aby wykonać
- * to zadanie.
- * - Poproś użytkownika o podanie liczby całkowitej dodatniej N.
- * - Zainicjuj zmienną przechowującą sumę na 0.
- * - Użyj pętli while, aby dodawać kolejne liczby naturalne do sumy.
- * - Po osiągnięciu liczby N wyświetl sumę na ekranie.
- */
 int main() {
   short userValueN, result = 0, i;
   const short BEGIN_VALUE = 1;
--- lab2/petla_for/task5.cpp
+++ lab2/petla_for/task5.cpp
@@ -1,16 +1,5 @@
 #include <iostream>
 
-/*
- * Napisz program, który policzy, ile liczb parzystych
- * znajduje się w zakresie od 1 do N, gdzie N będzie liczbą
- * podaną przez użytkownika. Użyj pętli do-while, aby wykonać to zadanie.
- * - Poproś użytkownika o podanie liczby całkowitej dodatniej N.
- * - Zainicjuj zmienną przechowującą licznik parzystych na 0
- *   oraz zmienną pomocniczą do iteracji, rozpoczynając od 1.
- * - Użyj pętli do-while, aby sprawdzać każdą liczbę w zakresie od 1 do N:
- * - Jeśli liczba jest parzysta, zwiększ licznik parzystych o 1.
- * - Po zakończeniu pętli wyświetl liczbę liczb parzystych na ekranie.
- */
 int main() {
   short userValue;
   const short BEGIN_VALUE = 1;
