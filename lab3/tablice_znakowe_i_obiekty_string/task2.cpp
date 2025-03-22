a63582b225dea0c2b0da2a53adb3c1f0b4ca285c
Daniel Molenda <kontakt@dmolenda.it>
2025-03-22 10:27:51 +0100

    🔧 Add task3

--- lab3/tablice_znakowe_i_obiekty_string/task2.cpp
+++ lab3/tablice_znakowe_i_obiekty_string/task2.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+/*
+ * Napisz program, który wczyta od użytkownika ciąg znaków o
+ * długości maksymalnie 20 znaków, a następnie zamieni wszystkie
+ * wystąpienia litery 'a' na literę 'o'. Program wyświetli zmodyfikowany ciąg
+ * znaków. Kroki do wykonania:
+ * - Program wczytuje ciąg znaków od użytkownika i zapisuje go w tablicy o
+ * rozmiarze 21.
+ * - Program przechodzi przez każdy znak w tablicy, sprawdzając, czy jest to
+ * litera 'a'.
+ * - Jeśli znajdzie literę 'a', zamienia ją na literę 'o'.
+ * - Na końcu program wyświetla zmodyfikowany ciąg znaków.
+ */
+
 int main() {
   const int SIZE = 21;
   char buffer[SIZE];
---
+++ lab3/tablice_znakowe_i_obiekty_string/task3.cpp
@@ -0,0 +1,31 @@
+#include <cstring>
+#include <iostream>
+
+int main() {
+  std::string exampleString;
+  char charToCount;
+  int count = 0, stringLength = 0;
+
+  std::cout << "Podaj ciag znakow: ";
+  std::getline(std::cin, exampleString);
+  std::cout << "Podaj znak do zliczenia: ";
+  std::cin >> charToCount;
+  std::cout << "Liczba wystapien: '" << charToCount << "': ";
+  stringLength = exampleString.length();
+  for (int i = 0; i < stringLength; i++) {
+    if (exampleString[i] == charToCount) {
+      count++;
+    }
+  }
+  std::cout << count << std::endl;
+  std::cout << "Pozycje wystapien: ";
+  for (int i = 1; i < stringLength; i++) {
+    if (exampleString[i] == charToCount) {
+      std::cout << i;
+      if (i != stringLength - 1) {
+        std::cout << ", ";
+      }
+    }
+  }
+  std::cout << std::endl;
+}
