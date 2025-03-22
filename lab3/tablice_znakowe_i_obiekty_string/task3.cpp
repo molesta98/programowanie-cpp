6169b102abea73b8250d5838eadccf9a492118fc
Daniel Molenda <kontakt@dmolenda.it>
2025-03-22 10:45:24 +0100

    🔧 Task3 - done

--- lab3/tablice_znakowe_i_obiekty_string/task3.cpp
+++ lab3/tablice_znakowe_i_obiekty_string/task3.cpp
@@ -21,20 +21,24 @@
 int main() {
   std::string exampleString;
   char charToCount;
-  int count = 0, stringLength = 0;
+  int counter = 0, stringLength = 0;
 
   std::cout << "Podaj ciag znakow: ";
   std::getline(std::cin, exampleString);
+
   std::cout << "Podaj znak do zliczenia: ";
   std::cin >> charToCount;
+
   std::cout << "Liczba wystapien: '" << charToCount << "': ";
+
   stringLength = exampleString.length();
   for (int i = 0; i < stringLength; i++) {
     if (exampleString[i] == charToCount) {
-      count++;
+      counter++;
     }
   }
-  std::cout << count << std::endl;
+
+  std::cout << counter << std::endl;
   std::cout << "Pozycje wystapien: ";
   for (int i = 1; i < stringLength; i++) {
     if (exampleString[i] == charToCount) {
