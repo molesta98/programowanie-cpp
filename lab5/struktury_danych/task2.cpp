4891b5a93a6d2d2b5b8c33033155534a1362dbb4
Daniel Molenda <kontakt@dmolenda.it>
2025-05-26 13:29:41 +0200

    🔧 Add task1

---
+++ lab5/struktury_danych/task2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+struct Osoba {
+  std::string imie;
+  std::string nazwisko;
+  short wiek;
+};
+
+int main() {
+  const int SIZE = 3;
+  Osoba Osoba[SIZE] = {{"Daniel", "Molenda", 27},
+                       {"Jan", "Nowak", 30},
+                       {"Piotr", "Wiśniewski", 40}};
+
+  std::cout << "Dane zapisane w tablicy struktur:" << std::endl;
+  for (int i = 0; i < 3; i++) {
+    std::cout << "Imię: " << Osoba[i].imie
+              << ", Nazwisko: " << Osoba[i].nazwisko << ", Wiek; "
+              << Osoba[i].wiek << std::endl;
+  }
+  return 0;
+}
