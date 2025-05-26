98ced09ff105b64e6219814618b1ed3b54268d14
Daniel Molenda <kontakt@dmolenda.it>
2025-05-26 22:01:49 +0200

    🔧 Add task1, task2, task3

---
+++ lab5/wskazniki/task3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int ObliczSume(int *array, int size) {
+  int sum = 0;
+  for (int i = 0; i < size; ++i) {
+    sum += *(array + i);
+  }
+  return sum;
+}
+
+int main() {
+  const int SIZE = 4;
+  int exampleArray[SIZE] = {1, 1, 1, 1};
+
+  int sum = ObliczSume(exampleArray, SIZE);
+  std::cout << "Suma elementow tablicy: " << sum << std::endl;
+
+  for (int i = 0; i < SIZE; i++) {
+    std::cout << "Podaj nowa wartosc dla elementu " << i << ": ";
+    std::cin >> *(exampleArray + i);
+  }
+
+  std::cout << "Nowa tablica: ";
+  for (int i = 0; i < SIZE; i++) {
+    std::cout << *(exampleArray + i) << " ";
+  }
+  std::cout << std::endl;
+
+  sum = ObliczSume(exampleArray, SIZE);
+  std::cout << "Nowa suma elementow tablicy: " << sum << std::endl;
+}
