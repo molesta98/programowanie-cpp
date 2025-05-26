df2c000394a8bba9ab869e3b4d019d3269da2116
Daniel Molenda <kontakt@dmolenda.it>
2025-05-26 21:00:41 +0200

    💡 Remove comments

--- lab2/petla_while_i_do_while/task3.cpp
+++ lab2/petla_while_i_do_while/task3.cpp
@@ -1,19 +1,5 @@
 #include <iostream>
 
-/*
- * Napisz program, który wczyta liczby od użytkownika,
- * aż ten zdecyduje się zakończyć wprowadzanie. Użyj pętli do-while, aby wykonać
- * to zadanie.
- * - Zainicjuj zmienną do przechowywania liczby.
- * - W pętli do-while:
- *    - Poproś użytkownika o podanie liczby.
- *    - Wyświetl wprowadzone liczby na ekranie.
- *    - Zapytaj użytkownika, czy chce kontynuować (np. 'Czy chcesz wprowadzić
- *      kolejną liczbę? (t/n)').
- * - Kontynuuj pętlę, jeśli użytkownik wybierze 't', a zakończ, jeśli wybierze
- * 'n'.
- */
-
 int main() {
   short number;
   char decision;
