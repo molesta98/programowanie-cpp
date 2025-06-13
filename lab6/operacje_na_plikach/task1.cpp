bf38eda483545a7934ca25d61775366372c78f84
Daniel Molenda <mail@dmolenda.it>
2025-06-13 18:21:38 +0200

    🐛 Fix typos

--- lab6/operacje_na_plikach/task1.cpp
+++ lab6/operacje_na_plikach/task1.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main() {
-  char *imie, nazwisko;
+  std::string imie, nazwisko;
   short wiek;
 
   std::ifstream plik("dane.txt");
@@ -10,7 +10,6 @@ int main() {
     std::cout << "Nie można otworzyć pliku!" << std::endl;
     return 1;
   }
-  char* linia;
 
   while (plik >> imie >> nazwisko >> wiek) {
     std::cout << "Imię: " << imie << ", Nazwisko: " << nazwisko
